3077d0bc219595a95c565123fbbc0150b8837d9f
gustavosantos <gustavogimenezs@icloud.com>
2019-06-19 22:22:56 -0300

    Primeiro commit

---
+++ Automacao/OrientacaoObjeto/OrientacaoObjetos.rb
@@ -0,0 +1,33 @@
+class ClassName
+
+    #Equivalente ao get set Java
+    attr_accessor: nome
+
+    #attr_reader: nomeum
+
+    #attr_writer: nomedois
+
+=begin
+    #Get
+    def nome
+        @nome
+    end
+
+    #Set
+    def nome(nome)
+        @nome = :nome
+    end
+=end
+
+def method_name
+    #Corpo do metodo
+end
+
+
+
+
+end
+
+objeto = ClassName.new
+
+objeto.nome = ""
