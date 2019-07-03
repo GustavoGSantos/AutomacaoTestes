d4943dbe9a3396f1e3d5367208f2f33f6d970e52
gustavosantos <gustavogimenezs@icloud.com>
2019-07-03 17:35:29 -0300

    Configurando Capybara para abrir em tela maximizada para não dar erro no teste

--- Automacao/CapybaraTeste/features/step_definitions/buscandoElementos.rb
+++ Automacao/CapybaraTeste/features/step_definitions/buscandoElementos.rb
@@ -10,5 +10,5 @@ Então("verifico se encontrei os elementos") do
     find_by_id('teste')
     find_button(class: 'btn waves-light')
     first('.btn')
-    find_link(href: 'https://www.youtube.com/channel/UCp554v_is_ZzjzSmAQyFfAA')
+    find_link(href: 'https://www.youtube.com/channel/UCp554v_is_ZzjzSmAQyFfAA') 
 end  
--- Automacao/CapybaraTeste/features/support/env.rb
+++ Automacao/CapybaraTeste/features/support/env.rb
@@ -6,4 +6,5 @@ Capybara.configure do |config|
     config.default_driver = :selenium_chrome #Configurando driver
     config.app_host = 'https://automacaocombatista.herokuapp.com' #Configurando url padrão
     config.default_max_wait_time = 5 #Tempo de espera padrão
+    Capybara.page.driver.browser.manage.window.maximize
 end
