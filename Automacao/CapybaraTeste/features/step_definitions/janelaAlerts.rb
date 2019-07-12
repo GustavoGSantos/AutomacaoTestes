ccf6612628548d70549c5d464cc4c24f232e8983
gustavosantos <gustavogimenezs@icloud.com>
2019-07-11 22:37:58 -0300

    Criando teste para interação com alerts

--- Automacao/CapybaraTeste/features/step_definitions/janelaAlerts.rb
+++ Automacao/CapybaraTeste/features/step_definitions/janelaAlerts.rb
@@ -7,6 +7,7 @@ Quando("eu entro na janela e verifico a mensagem") do
         expect(current_url).to eq 'https://automacaocombatista.herokuapp.com/mudancadefoco/newwindow'
         @mensagem = find('.text-darken-1')
         expect(@mensagem.text).to eq 'Você Abriu uma nova janela!!'
+        sleep(3)
         janela.close
     end
 
@@ -16,9 +17,22 @@ Quando("eu entro na janela e verifico a mensagem") do
     expect(current_url).to eq 'https://automacaocombatista.herokuapp.com/mudancadefoco/newwindow'
     @mensagem = find('.text-darken-1')
     expect(@mensagem.text).to eq 'Você Abriu uma nova janela!!'
+    sleep(3)
     windows.last.close
 end
   
 Quando("eu entro no alert e verifico faco a acao") do
-    
+    visit '/mudancadefoco/alert'
+    find('button[onclick="jsAlert()"]').click
+    sleep(3)
+    page.accept_alert
+
+    find('button[onclick="jsConfirm()"]').click
+    sleep(3)
+    page.dismiss_confirm
+
+    find('button[onclick="jsPrompt()"]').click
+    sleep(3)
+    page.accept_prompt(with: 'Gustavo Santos')
+    sleep(3)
 end  
