8ae6c41c07b535e66e36ff111911ae8abb6b797d
gustavosantos <gustavogimenezs@icloud.com>
2019-07-23 20:49:08 -0300

    Criando os códigos necessários para utilizar o site prism

---
+++ Automacao/SitePrism/features/support/env.rb
@@ -0,0 +1,10 @@
+require 'capybara/cucumber'
+require 'capybara'
+
+Capybara.configure do |config|
+    #selenium selenium_chrome selenium_chrome_headless(para rodar no jnekins, roda sem browser)
+    config.default_driver = :selenium_chrome #Configurando driver
+    config.app_host = 'https://automacaocombatista.herokuapp.com' #Configurando url padrão
+    config.default_max_wait_time = 5 #Tempo de espera padrão
+    Capybara.page.driver.browser.manage.window.maximize
+end
